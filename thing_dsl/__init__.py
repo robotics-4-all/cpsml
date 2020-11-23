ce9b0815cda24b83d121e65cf5132876a2e23ea6
Konstantinos Panayiotou <klpanagi@gmail.com>
2020-11-24 00:54:11 +0200

    .

--- thing_dsl/__init__.py
+++ thing_dsl/__init__.py
@@ -1,17 +1,13 @@
 import os
 from textx import language, metamodel_from_file
 
+from .utils import get_mm
+
 __version__ = "0.1.0.dev"
 
 
 @language('thing_dsl', '*.thing')
 def thing_dsl_language():
     "thing_dsl language"
-    current_dir = os.path.dirname(__file__)
-    mm = metamodel_from_file(os.path.join(current_dir, 'thing_dsl.tx'))
-
-    # Here if necessary register object processors or scope providers
-    # http://textx.github.io/textX/stable/metamodel/#object-processors
-    # http://textx.github.io/textX/stable/scoping/
-
+    mm = get_mm()
     return mm
