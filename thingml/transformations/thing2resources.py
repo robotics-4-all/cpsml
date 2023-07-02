29029cbb76ba4096b2d280ca63202aaa58f67eb8
Konstantinos Panayiotou <klpanagi@gmail.com>
2023-07-02 15:17:30 +0300

    Work on Things2Resources M2M

--- thingml/transformations/thing2resources.py
+++ thingml/transformations/thing2resources.py
@@ -3,15 +3,55 @@ from os.path import basename
 from thingml.utils import get_thing_mm
 
 
-def build_resource(name, rtype, interface, namespace='', uri=''):
+def build_single_resource(name, rtype, interface, namespace='', uri=''):
     txt = f"""
     Resource<{rtype}> {name}
-        'uri': '{uri}'
-        'interface': {interface}
-        'namespace': '{namespace}'
+        uri: '{uri}'
+        interface: {interface}
+        namespace: '{namespace}'
     end
     """
-    print(txt)
+    return txt
+
+
+def build_thing_resources(thing):
+    print(f'[*] Installed Sensors:')
+    for sensor in thing.sensors:
+        print(f'- {sensor.name}: {sensor.__class__.__name__}')
+    print(f'[*] Installed Actuators:')
+    for actuator in thing.actuators:
+        print(f'- {actuator.name}: {actuator.__class__.__name__}')
+    print(f'[*] Installed Computation Boards:')
+    for board in thing.boards:
+        print(f'- {board}')
+    txt = ''
+    for sensor in thing.sensors:
+        txt += build_single_resource(
+            sensor.name, 'Sense',
+            f'Publisher<{sensor.dataModel.name}>',
+            uri=f'{thing.name.lower()}.sensors.{sensor.name.lower()}'
+        )
+    for actuator in thing.actuators:
+        txt += build_single_resource(
+            actuator.name,
+            'Act',
+            f'Subscriber<{actuator.dataModel.name}>',
+            uri=f'{thing.name.lower()}.actuators.{actuator.name.lower()}'
+        )
+    # for cap in thing.capabilities:
+    #     txt += build_single_resource(
+    #         cap,
+    #         'Compute',
+    #         f'Subscriber<{cap}>',
+    #         uri=f'{thing.name.lower()}.{cap.lower()}'
+    #     )
+    return txt
+
+
+def build_resources_model_file(resources: str, filename='resources'):
+    filepath = f'{filename}.resource'
+    with open(filepath, 'w') as fp:
+        fp.write(resources)
 
 
 def t2r_m2m(thing_model, output_model=''):
@@ -26,31 +66,9 @@ def t2r_m2m(thing_model, output_model=''):
         print('---------------------------------------')
         if thing.__class__.__name__ == 'Robot':
             print(f'[*] Found {thing.__class__.__name__} model: {thing.name}')
-            print(f'[*] Installed Sensors:')
-            for sensor in thing.sensors:
-                print(f'- {sensor.name}: {sensor.__class__.__name__}')
-            print(f'[*] Installed Actuators:')
-            for actuator in thing.actuators:
-                print(f'- {actuator.name}: {actuator.__class__.__name__}')
-            print(f'[*] Installed Computation Boards:')
-            for board in thing.boards:
-                print(f'- {board}')
-            for sensor in thing.sensors:
-                build_resource(sensor.name, 'Sense',
-                               f'Publisher<{sensor.dataModel.name}>',
-                               uri=f'{thing.name.lower()}.sensors.{sensor.name.lower()}'
-                               )
-            for actuator in thing.actuators:
-                build_resource(actuator.name,
-                               'Act',
-                               f'Subscriber<{actuator.dataModel.name}>',
-                               uri=f'{thing.name.lower()}.actuators.{actuator.name.lower()}'
-                               )
-            for cap in thing.capabilities:
-                build_resource(cap,
-                               'Compute',
-                               f'Subscriber<{cap}>',
-                               uri=f'{thing.name.lower()}.{cap.lower()}'
-                               )
+            resources = build_thing_resources(thing)
+            build_resources_model_file(resources, thing.name)
         elif thing.__class__.__name__ == 'Device':
             print(f'[*] Found {thing.__class__.__name__} model: {thing.name}')
+            resources = build_thing_resources(thing)
+            build_resources_model_file(resources, thing.name)
